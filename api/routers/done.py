9b19169759854aa29d021993d7f4a76f124aca7c
oyu <12345yuu6789@gmail.com>
2025-01-30 23:13:18 +0900

    routerの中身を追加

--- api/routers/done.py
+++ api/routers/done.py
@@ -1,13 +1,14 @@
 from fastapi import APIRouter
+from fastapi.responses import Response
 
 
 router = APIRouter()
 
+# putやdeleteメソッドでresponseが必要ない場合、returnで何も返さない
+@router.put("/task/{task_id}/done", tags=["done"], response_model=None)
+async def mark_task_as_done(task_id: int):
+  return Response(status_code=204)
 
-@router.put("/task/{task_id}/done", tags=["done"])
-async def mark_task_as_done():
-  pass
-
-@router.delete("/task/{task_id}/done", tags=["done"])
-async def unmark_task_as_done():
-  pass
+@router.delete("/task/{task_id}/done", tags=["done"], response_model=None)
+async def unmark_task_as_done(task_id: int):
+  return Response(status_code=204)
--- api/schemas/task.py
+++ api/schemas/task.py
@@ -1,7 +1,24 @@
 from pydantic import BaseModel, Field
 
 
-class Task(BaseModel):
-  id: int
+class TaskBase(BaseModel):
   title: str | None = Field(default=None, example="クリーニングに行く")
+
+
+class Task(TaskBase):
+  id: int
   done: bool = Field(default=False, description="完了フラグ")
+  
+  class config:
+    orm_mode = True
+  
+
+class TaskCreate(TaskBase):
+  pass
+
+
+class TaskCreateResponse(TaskBase):
+  id: int
+  
+  class config:
+    orm_mode = True
