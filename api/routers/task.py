98d5061ea80af71dea965fd03ea40101747e084e
oyu <12345yuu6789@gmail.com>
2025-01-30 17:18:52 +0900

    schemaの追加

--- api/routers/task.py
+++ api/routers/task.py
@@ -1,12 +1,13 @@
 from fastapi import APIRouter
+import api.schemas.task as task_schema
 
 
 router = APIRouter()
 
 
-@router.get("/tasks", tags=["task"])
+@router.get("/tasks", tags=["task"], response_model=list[task_schema.Task])
 async def list_tasks():
-    pass
+    return [task_schema.Task(id=1, title="1つ目のTodoタスク")]
 
 @router.post("/tasks", tags=["task"])
 async def create_task():
