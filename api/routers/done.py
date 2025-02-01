bb497a7edd22449a161213e3256d1dbfdedc9f2b
oyu <12345yuu6789@gmail.com>
2025-02-01 16:01:42 +0900

    loop warningが解消できなかったけど、testはpassできた

--- api/routers/done.py
+++ api/routers/done.py
@@ -9,17 +9,17 @@ from api.models.task import get_db
 router = APIRouter()
 
 # putやdeleteメソッドでresponseが必要ない場合、returnで何も返さない
-@router.put("/task/{task_id}/done", tags=["done"], response_model=done_schema.DoneResponse)
+@router.put("/tasks/{task_id}/done", tags=["done"], response_model=done_schema.DoneResponse)
 async def mark_task_as_done(task_id: int, db: AsyncSession = Depends(get_db)):
   done = await done_crud.get_done(db, task_id=task_id)
   if done is not None:
-    raise HTTPException(status_code=404, detail="Done already exists")
+    raise HTTPException(status_code=400, detail="Done already exists")
   
   return await done_crud.create_done(db, task_id)
   
   
 # 完了フラグを消す
-@router.delete("/task/{task_id}/done", tags=["done"], response_model=None)
+@router.delete("/tasks/{task_id}/done", tags=["done"], response_model=None)
 async def unmark_task_as_done(task_id: int, db: AsyncSession = Depends(get_db)):
   done = await done_crud.get_done(db, task_id=task_id)
   if done is None:
